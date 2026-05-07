ce9bb4f919a04ff49ebc44267624b71f41e333f5
RobertoDias <ra48273551857@acaonsfatima.org.br>
2026-05-07 19:18:54 -0300

    endpoint

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Olá,Mundo")
+import express from "express";
+const servidor = express()
+
+servidor.get("/helloworld", (rec, post) => {
+    post.send("Hello, World")
+})
+
+servidor.get("/helloworld/boas", (rec, post) => {
+
+    post.send("boaaa")
+})
+
+servidor.get("/calculadora/:n1/:n2", (rec, resp) => {
+    let n1 = Number(rec.params.n1)
+    let n2 = Number(rec.params.n2)
+    let resposta = n1 + n2
+    resp.send("A soma é " + resposta)
+})
+
+servidor.listen(6767, () => console.log(" Farmei Aura "))
